e3f4c20b95b512c165e57d9e338b31fbadf64973
nikic8p <120654238+nikic8p@users.noreply.github.com>
2025-01-13 11:40:41 +0100

    zahlenmatrix

---
+++ Labor/zahlenmatrix.py
@@ -0,0 +1,17 @@
+import cv2 #Importieren OpenCV
+import numpy as np #Importieren Numpy
+
+image=cv2.imread('../Bilder/greyscale.tiff') #Bild laden
+w, h = image.shape[0:2]
+
+
+x = int(input("X Wert: "))
+y = int(input("Y Wert: "))
+
+print(x, y)
+
+value = image[x, w, 0]
+
+print(type(value))
+
+print("BGR Werte: ", image[x, w])
