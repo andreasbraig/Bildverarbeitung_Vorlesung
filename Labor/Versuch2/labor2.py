ea819ed2ac1b77e76c37513524b9db5094a6cf93
andreasbraig <jpotze22@gmail.com>
2025-01-23 15:00:37 +0100

    Labor 2 gelöst unkommentiert

--- Labor/Versuch2/labor2.py
+++ Labor/Versuch2/labor2.py
@@ -1,26 +1,62 @@
 import cv2 #Importieren OpenCV
 import numpy as np #Importieren Numpy
+from matplotlib import pyplot as plt
 
-image=cv2.imread('../../Bilder/sample1_lowcontrast.jpg') #Bild laden (im unterordner)
-
-
-def kontrastspreizung(image):
-
-    #Kontrastanalyse 
+image=cv2.imread('../../Bilder/sample1.jpg') #Bild laden (im unterordner)
 
 
+def global_kontrastspeizung(image):
 
-    #kontrastberechnung
-
-    for height in image:
-
-        newimage = np.matrix(height)
-        print(newimage)
+    #bestimme die maximale Dehnung des vorhandenen Bildes
+    maxpos = np.max(image.copy())
+    minpos = np.min(image.copy())
 
 
-    #zu Bild zusammenführen
+    f = (255/(maxpos-minpos))
 
-    
+    #berechne nach Formel aus Vorlesung
+    img  = np.uint8(((image - minpos) * (f)))
+
+    return img
+
+
+
+def kanal_kontrastspeizung(image):
+
+    #teile das Bild in Schwarz weiß fotos der einzelnen Kanäle
+    b,g,r =cv2.split(image)
+
+    #merge und führe Globale spreizung für jedes Foto durch 
+    result = cv2.merge([global_kontrastspeizung(b),global_kontrastspeizung(g),global_kontrastspeizung(r)])
+
+    return result
+
+def pseudokolorierung(image):
+
+
+    b = np.uint8(np.sin((2*np.pi/255)*image)*255)
+    g = np.uint8(np.cos((2*np.pi/255)*image)* -1 *255)
+    r = np.uint8(np.sin((2*np.pi/255)*image)* -1 *255)
+
+    newimage = cv2.merge([b,g,r])
+
+    return newimage
+
+def whitebalance(image):
+
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    result = cv2.minMaxLoc(gray)
+
+    print(result[3][0])
+
+    b,g,r = cv2.split(image)
+
+    b = b * (255/image[result[3][0]][result[3][1]])[0]
+    g = g * (255/image[result[3][0]][result[3][1]])[0]
+    r = r * (255/image[result[3][0]][result[3][1]])[0]
+
+    newimage = cv2.merge(np.uint8([b,g,r]))
 
     return newimage
 
@@ -32,4 +68,4 @@ def show_image(text, image):
     cv2.destroyAllWindows()
 
 
-kontrastspreizung(image)
+show_image("test",whitebalance(image))
